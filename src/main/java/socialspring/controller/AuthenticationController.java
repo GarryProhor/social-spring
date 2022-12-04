7f2a23a8cb00d85f01430f86895b1eaa39c5bdfa
GarryProhor <igorprohorchenko@gmail.com>
2022-12-04 21:24:48 +0300

    comment to controller for request

--- src/main/java/socialspring/controller/AuthenticationController.java
+++ src/main/java/socialspring/controller/AuthenticationController.java
@@ -16,27 +16,39 @@ import java.util.LinkedHashMap;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-public class  AuthenticationController {
+public class AuthenticationController {
     private final UserService userService;
 
     @ExceptionHandler({EmailAlreadyTakenException.class})
-    public ResponseEntity<String> handlerEmailTaken(){
+    public ResponseEntity<String> handlerEmailTaken() {
         return new ResponseEntity<>("The email you provided is already", HttpStatus.CONFLICT);
     }
 
-    // go to http://localhost:8080/auth/register
+    //    POST localhost:8080/auth/register
+//    {
+//            "firstName" : "abed",
+//            "lastName" : "abed",
+//            "email" : "test@test.ru",
+//            "dob": "2022-12-03"
+//    }
     @PostMapping("/register")
-    public ApplicationUser registerUser(@RequestBody RegistrationObject regObject){
+    public ApplicationUser registerUser(@RequestBody RegistrationObject regObject) {
         return userService.registerUser(regObject);
     }
 
     @ExceptionHandler({UserDoesNotException.class})
-    public ResponseEntity<String> handleUserDoesntExist(){
+    public ResponseEntity<String> handleUserDoesntExist() {
         return new ResponseEntity<>("The user you are looking for doesnt exist", HttpStatus.NOT_FOUND);
     }
 
+//    PUT localhost:8080/auth/update/phone
+//    {
+//        "userName": "abedabed663910176",
+//        "phone": "123456789"
+//    }
+
     @PutMapping("/update/phone")
-    public ApplicationUser updatePhoneNumber(@RequestBody LinkedHashMap<String , String> body){
+    public ApplicationUser updatePhoneNumber(@RequestBody LinkedHashMap<String, String> body) {
         String username = body.get("userName");
         String phone = body.get("phone");
 
@@ -47,12 +59,16 @@ public class  AuthenticationController {
     }
 
     @ExceptionHandler({EmailFailedToSendException.class})
-    public ResponseEntity<String> handleFailedEmail(){
+    public ResponseEntity<String> handleFailedEmail() {
         return new ResponseEntity<>("Email failed to send, try again in a moment", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    //    POST localhost:8080/auth/email/code
+//    {
+//        "userName": "abedabed663910176"
+//    }
     @PostMapping("/email/code")
-    public ResponseEntity<String> createEmailVerification(@RequestBody LinkedHashMap<String, String> body){
+    public ResponseEntity<String> createEmailVerification(@RequestBody LinkedHashMap<String, String> body) {
         userService.generateEmailVerification(body.get("userName"));
         return new ResponseEntity<>("Verification code generated email sent", HttpStatus.OK);
     }
