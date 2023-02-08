7f79ff3eb21136464c22e5b8e1cbe2b3cc5efd4d
GarryProhor <igorprohorchenko@gmail.com>
2023-02-08 15:15:22 +0300

    setup encoders and decoders for JWT tokens

--- src/main/java/socialspring/config/RSAKeyProperties.java
+++ src/main/java/socialspring/config/RSAKeyProperties.java
@@ -1,11 +1,12 @@
 package socialspring.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
-
+@Configuration
 @ConfigurationProperties(prefix="rsa")
 public class RSAKeyProperties {
     private RSAPublicKey publicKey;
--- src/main/java/socialspring/config/SecurityConfiguration.java
+++ src/main/java/socialspring/config/SecurityConfiguration.java
@@ -1,14 +1,38 @@
 package socialspring.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
+import socialspring.service.UserService;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private  final RSAKeyProperties keys;
+    private final UserService userService;
+
+    @Autowired
+    public SecurityConfiguration(RSAKeyProperties keys, UserService userService) {
+        this.keys = keys;
+        this.userService = userService;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -16,10 +40,32 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService detailsService){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(detailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf().disable();
-        httpSecurity.authorizeRequests().anyRequest().permitAll();
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> auth
+                        .mvcMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
 
-        return httpSecurity.build();
+    @Bean
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(keys.getPublicKey()).build();
+    }
+    @Bean
+    JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(keys.getPublicKey()).privateKey(keys.getPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
