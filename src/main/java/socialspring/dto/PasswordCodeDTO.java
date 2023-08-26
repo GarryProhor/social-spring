b58fa795e79983990a62874ab00aea8bb50dbf9f
GarryProhor <igorprohorchenko@gmail.com>
2023-08-26 09:35:45 -0800

    ADD: PasswordCodeDTO

---
+++ src/main/java/socialspring/dto/PasswordCodeDTO.java
@@ -0,0 +1,15 @@
+package socialspring.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PasswordCodeDTO {
+
+    int code;
+    String email;
+}
